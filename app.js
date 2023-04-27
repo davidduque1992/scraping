1f3c25376c85cbb140d561709e3f645e9d3b55d9
DavidDuque <105513600+davidduque1992@users.noreply.github.com>
2023-04-27 13:59:22 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
 const randomUseragent = require("random-useragent");
 const fs = require("fs");
+
 const init = async () => {
+  // Configurar Puppeteer
   const header = randomUseragent.getRandom((ua) => {
     return ua.browserName === "Firefox";
   });
@@ -17,16 +19,36 @@ const init = async () => {
   await page.setUserAgent(header);
 
   await page.setViewport({ width: 1366, height: 768 });
+
+  // Leer cookies
   try {
     const readCookies = fs.readFileSync("./cookies.txt", "utf8");
     if (readCookies.length > 0) {
-      const readCookies = fs.readFileSync("./cookies.txt", "utf8");
       const parseCookies = JSON.parse(readCookies);
       await page.setCookie(...parseCookies);
-      await page.goto("https://www.linkedin.com/company/bendoapp");
-    //   await page.waitForNavigation();
-    //   await browser.close();
+      await page.goto(
+        "https://www.linkedin.com/jobs/search/?f_AL=true&f_WT=2&geoId=103323778&keywords=Node%20Js%20React%20Js&location=M%C3%A9xico&refresh=true&sortBy=R"
+      );
       await page.screenshot({ path: "example.png" });
+
+      // Obtener data-job-id de cada elemento div
+      const jobIds = await page.$$eval("div[data-job-id]", (divs) => {
+        return divs.map((div) => div.getAttribute("data-job-id"));
+      });
+
+      // Crear objeto con id de job y si tiene solicitud sencilla
+      const jobs = {};
+      for (const jobId of jobIds) {
+        const hasEasyApply = await page.evaluate(
+          (jobId) =>
+            !!document.querySelector(
+              `div[data-job-id="${jobId}"] span.artdeco-button__text`
+            ),
+          jobId
+        );
+        jobs[jobId] = { hasEasyApply };
+      }
+      console.log(jobs);
     } else {
       console.log("No hay cookies");
     }
@@ -36,10 +58,9 @@ const init = async () => {
       "https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
     );
 
-    await page.type("#username", "<tu usuario>");
-    await page.type("#password", "<tu passs>");
+    await page.type("#username", "davidduque1992@gmail.com");
+    await page.type("#password", "06/11/2011");
     await page.click('[data-litms-control-urn="login-submit"]');
-    //   await page.waitForNavigation();
 
     await page.waitForNavigation();
 
@@ -48,14 +69,11 @@ const init = async () => {
     await browser.close();
     init();
   }
-
-  //--------------------------------------------------------------------------segunda parte
-
-  //   await page.screenshot({ path: "example.png" });
 };
+
 init();
 
-saveCookies = (data) => {
+const saveCookies = (data) => {
   fs.writeFile("cookies.txt", JSON.stringify(data), (err) => {
     if (err) return console.log(err);
   });
