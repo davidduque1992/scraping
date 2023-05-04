869790d4985510021f41fca09a38a2f267fb98de
DavidDuque <105513600+davidduque1992@users.noreply.github.com>
2023-05-04 10:52:26 -0300

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const init = async () => {
 
   await page.setUserAgent(header);
 
-  await page.setViewport({ width: 1366, height: 768 });
+  await page.setViewport({ width: 1366, height: 5000 });
 
   // Leer cookies
   try {
@@ -32,21 +32,38 @@ const init = async () => {
       await page.screenshot({ path: "example.png" });
 
       // Obtener data-job-id de cada elemento div
-      const jobIds = await page.$$eval("div[data-job-id]", (divs) => {
-        return divs.map((div) => div.getAttribute("data-job-id"));
+      const jobs = await page.$$eval("div[data-job-id]", (divs) => {
+        return divs.map((div) => {
+          const jobId = div.getAttribute("data-job-id");
+          // const title = div.querySelector("h2")?.textContent;
+
+          return { jobId };
+        });
       });
+      console.log("43 jobs: ", jobs);
 
       // Crear objeto con id de job y si tiene solicitud sencilla
-      const jobs = {};
-      for (const jobId of jobIds) {
+      for (const job of jobs) {
         const hasEasyApply = await page.evaluate(
           (jobId) =>
             !!document.querySelector(
               `div[data-job-id="${jobId}"] span.artdeco-button__text`
             ),
-          jobId
+          job.jobId
         );
-        jobs[jobId] = { hasEasyApply };
+        const titulo = await page.evaluate(
+          (jobId) =>
+            !!document.querySelector(
+              `div[data-job-id="${jobId}"] span.artdeco-button__text`
+            ),
+          job.jobId
+        );
+
+        job.hasEasyApply = hasEasyApply;
+        job.link =
+          "https://www.linkedin.com/jobs/search/?currentJobId=" +
+          job.jobId +
+          "&f_AL=true&f_WT=2&geoId=103323778&keywords=Node%20Js%20React%20Js&location=M%C3%A9xico&refresh=true&sortBy=R";
       }
       console.log(jobs);
     } else {
